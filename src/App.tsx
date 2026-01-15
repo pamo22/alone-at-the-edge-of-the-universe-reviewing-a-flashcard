48e132d0cd0f5d922c49a3fec001e840143a65ab
Oscar Coghlan <oscar.coghlan@proton.me>
2026-01-15 19:31:50 +1100

    sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import AppHeader from './components/AppHeader.tsx'
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 import {Home, About, Contact} from './components/Views.tsx'
 
+function Heading() {
+  const location = useLocation();
+  switch(location.pathname) {
+    case '/':
+      return 'Dashboard';
+    case '/about':
+      return 'My Profile';
+    case '/contact':
+      return 'Settings';
+    default:
+      return 'Welcome';
+  };
+}
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
-    <>
-      <AppHeader heading="hello"/>
+      <AppHeader heading={Heading()}/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
+      <h1>DAHDUAHD</h1>
     </BrowserRouter>
   )
 }
+
 
 export default App
