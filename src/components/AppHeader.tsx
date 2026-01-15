40aca466dd23d3da91027a0028a79cda10913e70
Oscar Coghlan <oscar.coghlan@proton.me>
2026-01-15 15:32:05 +1100

    hell world >:)

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import flashcardLogo from '../assets/titlething.svg'
 import './AppHeader.css'
+import { Link } from 'react-router-dom';
 
 function AppHeader({heading = "Missing headeing"}) {
   return (
     <header>
       <img src={flashcardLogo} className="logo" alt="logo" />
       <h3>{heading}</h3>
-      {/* Navigation links or buttons */}
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
     </header>
   );
 }
---
+++ src/components/Views.tsx
@@ -0,0 +1,11 @@
+export function Home() {
+  return <h1>Home Page</h1>;
+}
+
+export function About() {
+  return <h1>About Page</h1>;
+}
+
+export function Contact() {
+  return <h1>Contact Page</h1>;
+}
